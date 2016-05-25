0362fd5e786d104b6b768474cf01ce6b59d1e34d
santhilatakv <santhilatakv@gmail.com>
2016-05-25 19:20:36 +0100

    New Algorithms

---
+++ src/main/java/EA/Decision.java
@@ -0,0 +1,13 @@
+package EA;
+
+/**
+ * Created by santhilata on 25/05/16.
+ */
+public class Decision extends TreeNode {
+
+    String classify;
+    public Decision(){
+        super();
+    }
+
+}
