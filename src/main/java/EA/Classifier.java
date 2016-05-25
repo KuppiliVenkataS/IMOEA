8c2d263d482fb2810b4e29979804ba4fd86a4958
santhilatakv <santhilatakv@gmail.com>
2016-05-25 19:33:43 +0100

    Classifiers

--- src/main/java/EA/Classifier.java
+++ src/main/java/EA/Classifier.java
@@ -1,7 +1,21 @@
 package EA;
 
+import java.util.ArrayList;
+
 /**
  * Created by santhilata on 25/05/16.
  */
 public class Classifier {
+    String attributeName;
+    Object dataType;
+    ArrayList data;
+
+
+    int numClasses;
+
+    public Object getMin(){
+        double min =0.0;
+        return min;
+    }
+
 }
