7a1b49f423eb4c14eb917d44d728c3e80ab8743d
santhilatakv <santhilatakv@gmail.com>
2016-05-26 16:47:52 +0100

    Check

--- src/main/java/DataInput/SampleInput.java
+++ src/main/java/DataInput/SampleInput.java
@@ -72,4 +72,44 @@ public class SampleInput {
 
         this.inputFile = inputFile;
     }//to test create data
+
+    /**
+     * The following is a sample raw data
+     * @throws IOException
+     */
+    public static File toTrainCreateData() throws IOException {
+
+        File inputFile = new File("raw_data_collected.csv");
+        FileWriter fw = new FileWriter(inputFile);
+        fw.flush();
+        fw.write("id,uLoc,cLoc,size,time,freq\n");
+        for (int qid = 0; qid < 100; qid++) { // 100 queries       => 0
+            fw.write(qid + ","); // enter qid
+            int uLoc = new Random().nextInt(6);//user location       => 1
+            while(uLoc == 0) uLoc = new Random().nextInt(6);
+            fw.write("uLoc_" + uLoc + ",");
+
+            int cLoc = new Random().nextInt(6); // cache location    =>2
+            while(cLoc == 0) cLoc = new Random().nextInt(6);
+            fw.write("cLoc_" + cLoc + ",");
+
+            int freq = new Random().nextInt(20); // frequency of a query   =>3
+            while(freq ==0)  freq = new Random().nextInt(20);
+            fw.write(freq + ",");
+
+            int timeWindows = new Random().nextInt(4);                // =>4
+            while (timeWindows == 0) timeWindows = new Random().nextInt(4);
+            fw.write(timeWindows + ",");
+
+            int dataSize = new Random().nextInt(20);             // =>5
+            while(dataSize ==0) dataSize = new Random().nextInt(20);
+            fw.write(dataSize + ",");
+
+            fw.write("\n");
+
+        }
+        fw.close();
+
+        return inputFile;
+    }//to test create data
 }
